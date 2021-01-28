f2033df6bda10b9d54a90323089a341d5cef7d78
luna <iszychen@gmail.com>
2021-01-28 13:58:50 +0800

    :memo: :tada:    config client

--- luna-config-client-node-I/src/main/java/com/luna/config/controller/ConfigClientController.java
+++ luna-config-client-node-I/src/main/java/com/luna/config/controller/ConfigClientController.java
@@ -1,10 +1,15 @@
 package com.luna.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author luna_mac
+ */
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")
